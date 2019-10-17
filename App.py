b8978633e3a4d4149a69aff2a1a4fb45578445f5
Lucas Santos <lucasgbi146@gmail.com>
2019-10-17 14:59:23 -0300

    coloquei outras rotas

--- App.py
+++ App.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/cadastro.html')
+def index():
+    return render_template('/paginas/cadastro.html')
+
+@app.route('/login.html')
+def index():
+    return render_template('/paginas/cadastro.html')
+
 if __name__=='__main__':
     app.run(debug=True)
